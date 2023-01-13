e795ccb552d83581e56852e79de28c657c7cf1ab
Anamul <ahasan192110@bscse.uiu.ac.bd>
2023-01-13 18:34:08 +0600

    login form

--- scr/screens/SignIn.js
+++ scr/screens/SignIn.js
@@ -1,6 +1,18 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TextInput, TouchableOpacity,image } from "react-native";
 
 export default class SgnIn extends React.Component{
     
+    state = {
+        name: ""
+    }
+    continue = () => {this.props.navigation.navigate("Chat", {name: this.state.name})}
+    render(){
+        return(
+            <View style={styles.viewStyle}>
+                <Text>Input User Name</Text>
+                <TextInput placeholder="User Name" onChange={name => {this.setState({name});}} value={this.stat.name} />
+            </View>
+        );
+    }
 } 
