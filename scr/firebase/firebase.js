a97b5968c82fa8687d501a612ecf3bf942e8266d
Anamul <ahasan192110@bscse.uiu.ac.bd>
2023-01-13 19:42:17 +0600

    message, messages and db ffunction created

--- scr/firebase/firebase.js
+++ scr/firebase/firebase.js
@@ -26,5 +26,34 @@ class Fire {
         });
     };
 
+    send = messages => {
+        messages.forEach(item => {
+            const message = {
+                text: item.text,
+                timestamp: firebase.database.ServerValue.TIMESTAMP,
+                user: item.user
+            }
+            this.db.push(message)
+        });
+
+    };
+
+    parse = message =>{
+        const{user, text, timestamp} = message.val();
+        const{key: _id} = message;
+        const createdAt = new Date(timestamp);
+
+        return{
+            _id,
+            createdAt,
+            text,
+            user
+        }
+    }
+
+    get db (){
+        return firebase.database().ref("messages");
+    }
+
     
 }
