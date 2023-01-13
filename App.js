fcc76dce2ebb52e870942ee9d94deaf40709b2ca
Anamul <ahasan192110@bscse.uiu.ac.bd>
2023-01-13 16:45:48 +0600

    firebase added

--- App.js
+++ App.js
@@ -1,5 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as firebase from 'firebase'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCp5gqjE93axYLjxLCX3-3NBKoT8MZINkU",
+  authDomain: "chatauth-47793.firebaseapp.com",
+  projectId: "chatauth-47793",
+  storageBucket: "chatauth-47793.appspot.com",
+  messagingSenderId: "487781291782",
+  appId: "1:487781291782:web:c81ec6ee9aa42c43e3f087"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig); 
 
 export default function App() {
   return (
