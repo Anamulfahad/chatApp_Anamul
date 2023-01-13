fa40d76117377464e8dccee2f9033f998ece10c1
Anamul <ahasan192110@bscse.uiu.ac.bd>
2023-01-13 20:15:59 +0600

    chat class created and componentDidMount function created

--- scr/screens/chat.js
+++ scr/screens/chat.js
@@ -1,2 +1,24 @@
 import React from "react";
 import { Platform, KeyboardAvoidingView, SafeAreaView  } from "react-native";
+import {GiftedChat} from 'react-native-gifted-chat';
+import Fire from "../firebase/firebase"
+
+export default class chat extends React.Component{
+    
+    state = {
+        messages: []
+    }
+
+    get user(){
+        return{
+            _id: Fire.uid,
+            name: this.props.navigation.state.params.name
+        }
+    }
+
+    componentDidMount(){
+        Fire.get(message => this.setState(previous=>({
+            messages: GiftedChat.append(previous.message, message)
+        })))
+    }
+} 
