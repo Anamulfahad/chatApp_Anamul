a368f199674262ae2273c9315a1724c28e4f3803
Anamul <ahasan192110@bscse.uiu.ac.bd>
2023-01-13 19:31:31 +0600

    check authority function created

--- scr/firebase/firebase.js
+++ scr/firebase/firebase.js
@@ -7,7 +7,24 @@ class Fire {
     }
     init = () => {
         if(!firebase.apps.length){
-            firebase.initializeApp
+            firebase.initializeApp({
+                apiKey: "AIzaSyCp5gqjE93axYLjxLCX3-3NBKoT8MZINkU",
+                authDomain: "chatauth-47793.firebaseapp.com",
+                projectId: "chatauth-47793",
+                storageBucket: "chatauth-47793.appspot.com",
+                messagingSenderId: "487781291782",
+                appId: "1:487781291782:web:c81ec6ee9aa42c43e3f087"
+            });
         }
-    }
+    };
+
+    checkAuth = () =>{
+        firebase.auth().onAuthStateChange(user => {
+            if(!user){
+                firebase.auth.signInAnonymously();
+            }
+        });
+    };
+
+    
 }
