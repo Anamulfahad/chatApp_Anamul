87eb77b4aee8746c32f910dd7f5f8f2a4b2979c4
Anamul <ahasan192110@bscse.uiu.ac.bd>
2023-01-13 17:41:25 +0600

    stack navigator for login and signup screen

--- App.js
+++ App.js
@@ -1,34 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import * as firebase from 'firebase'
-import 'firebase/firestore'
+import  {creatAppContainer} from 'react-navigation'
+import {createStackNavigator} from 'react-navigation-stack' 
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCp5gqjE93axYLjxLCX3-3NBKoT8MZINkU",
-  authDomain: "chatauth-47793.firebaseapp.com",
-  projectId: "chatauth-47793",
-  storageBucket: "chatauth-47793.appspot.com",
-  messagingSenderId: "487781291782",
-  appId: "1:487781291782:web:c81ec6ee9aa42c43e3f087"
-};
+import SignIn from './src/screens/SignIn'
+import SignIn from './src/screens/SignUp'
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig); 
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const appNavigator = createStackNavigator(
+  {
+    SignIn: SignIn,
+    SignUp: SignUp
   },
-});
+  {
+    headerNode: "none"
+  }
+)
+
+export default creatAppContainer(appNavigator);
