ef55092dbbdab160e3e0c4577fc32bf4a1b06509
Anamul <ahasan192110@bscse.uiu.ac.bd>
2023-01-13 19:20:26 +0600

    firebase installed and edited

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import  {creatAppContainer} from 'react-navigation'
 import {createStackNavigator} from 'react-navigation-stack' 
 
 import SignIn from './src/screens/SignIn'
-import SignIn from './src/screens/SignUp'
+import SignUp from './src/screens/SignUp'
 
 const appNavigator = createStackNavigator(
   {
---
+++ scr/firebase/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase'
+
+class Fire {
+    constructor(){
+        this.init()
+        this.checkAuth()
+    }
+    init = () => {
+        if(!firebase.apps.length){
+            firebase.initializeApp
+        }
+    }
+}
