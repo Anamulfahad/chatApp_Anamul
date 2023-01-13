992b0892d6adca641e4326366a31ffbc95d427e4
Anamul <ahasan192110@bscse.uiu.ac.bd>
2023-01-13 20:26:52 +0600

    chat screen created

--- scr/screens/chat.js
+++ scr/screens/chat.js
@@ -21,4 +21,20 @@ export default class chat extends React.Component{
             messages: GiftedChat.append(previous.message, message)
         })))
     }
+
+    componentWillUnmount(){
+        Fire.off();
+    }
+
+    render(){
+        const Chat = <GiftedChat messages={this.state.message} onSend = {Fire.send} user={this.user}/>
+        if (Platform === 'andro'){
+            <KeyboardAvoidingView style={{flex: 1}} behavior='padding' keyboardVerticalOffset={30} enabled>
+                {Chat}
+            </KeyboardAvoidingView>
+        }
+        return <SafeAreaView style={{flex: 1}}>
+            {Chat}
+        </SafeAreaView>
+    }
 } 
